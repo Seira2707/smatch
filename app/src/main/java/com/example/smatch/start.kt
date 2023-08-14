037074a2e367056b3f6b169a437693685437f079
hyunhomon <hyunhomon@gmail.com>
2023-08-14 15:23:24 +0900

    애니메이션 이슈 해결 및 프래그먼트 작업

--- app/src/main/java/com/example/smatch/start.kt
+++ app/src/main/java/com/example/smatch/start.kt
@@ -3,55 +3,39 @@ package com.example.smatch
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
-import java.util.Timer
-import kotlin.concurrent.timer
 
 class start : AppCompatActivity() {
 
     lateinit var logoLeft : ImageView
     lateinit var logoRight : ImageView
-//    val goleft = AnimationUtils.loadAnimation(this, R.anim.goleft)
-//    val goright = AnimationUtils.loadAnimation(this, R.anim.goright)
-    var time = 0
-    lateinit var timerTask : Timer
-    val intentProfile = Intent(this, profile::class.java)
-
-    fun startTimer() {
-        timerTask = timer(period = 100) {
-            time ++
-        }
-    }
-    fun resetTimer() {
-        timerTask ?.cancel()
-        time = 0
-    }
+    lateinit var goleft : Animation
+    lateinit var goright : Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
 
-        val logoLeft : ImageView = findViewById(R.id.logoleft)
-        val logoRight : ImageView = findViewById(R.id.logoright)
-        var timerTask : Timer? = null
+        val intentProfile = Intent(this, profile::class.java)
+        val countDown: CountDownTimer = object : CountDownTimer(1000, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                logoLeft.startAnimation(goright)
+                logoRight.startAnimation(goleft)
+            }
 
-        startTimer()
-
-//        logoLeft.startAnimation(goright)
-//        logoRight.startAnimation(goleft)
-
-        if(time == 25){
-            resetTimer()
-            startActivity(intentProfile)
+            override fun onFinish() {
+                startActivity(intentProfile)
+            }
         }
-    }
 
-    override fun onStart() {
-        super.onStart()
-    }
+        logoLeft = findViewById(R.id.logoleft)
+        logoRight = findViewById(R.id.logoright)
+        goleft = AnimationUtils.loadAnimation(this, R.anim.goleft)
+        goright = AnimationUtils.loadAnimation(this, R.anim.goright)
 
-    override fun onResume() {
-        super.onResume()
+        countDown.start()
     }
 }
